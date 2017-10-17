84fd39ffc738c60b951ae07200196fe58a894589
Randall Leeds <randall@apache.org>
2017-10-16 20:36:25 -0700

    Implement a TextQuoteSelector

---
+++ packages/text/index.js
@@ -0,0 +1,41 @@
+/**
+ * @license
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+import { createSelector } from '@annotator/selector';
+
+const contextSelector = context => context;
+
+export function createTextQuoteSelector(selectors) {
+  let patterns = selectors.map(({ exact }) => exact);
+
+  async function* exec(context) {
+    for (let pattern of patterns) {
+      let lastIndex = 0;
+      let next = () => context.indexOf(pattern, lastIndex);
+      let match = next();
+      while (match !== -1) {
+        let result = [pattern];
+        result.index = match;
+        result.input = context;
+        result.selector = pattern;
+        yield result;
+        lastIndex = match + 1;
+        match = next();
+      }
+    }
+  }
+
+  return createSelector(contextSelector, exec);
+}
