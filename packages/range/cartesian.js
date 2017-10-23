935b71b5bf032c9396b3f5100027c83a26e0bba3
Gerben <gerben@treora.com>
2017-10-23 01:09:21 -0700

    Make cartesian faster.
    Listen to all iterables in parallel, so that any combinations are
    yielded as soon as they could be available.

--- packages/range/cartesian.js
+++ packages/range/cartesian.js
@@ -1,19 +1,58 @@
-import { AsyncTee } from '@annotator/tee';
+import cartesianArrays from 'cartesian';
 
 export default cartesian;
 async function* cartesian(...iterables) {
-  if (iterables.length === 1) {
-    for await (let item of iterables[0]) {
-      yield [item];
+  // We listen to all iterators in parallel, while logging all the values they
+  // produce. Whenever an iterator produces a value, we produce and yield all
+  // combinations of that value with the logged values from other iterators.
+  // Every combination is thus made exactly once, and as soon as it is known.
+
+  const iterators = iterables.map(iterable => iterable[Symbol.asyncIterator]());
+  // Initialise an empty log for each iterable.
+  const logs = iterables.map(() => []);
+
+  const nextPs = iterators.map((iterator, iterableNr) =>
+    iterator
+      .next()
+      .then(async ({ value, done }) => ({ value: await value, done }))
+      .then(
+        // Label the value with iterableNr to know which one produced this value in
+        // Promise.race below.
+        ({ value, done }) => ({ value, done, iterableNr })
+      )
+  );
+
+  while (1) {
+    // Check which iterators are still active; quit if they are all exhausted.
+    const nextValuePs = nextPs.filter(x => x !== null);
+    if (nextValuePs.length === 0) break;
+
+    // Wait until any of the active iterators has produced a new value.
+    const { value, done, iterableNr } = await Promise.race(nextValuePs);
+
+    // If this iterable was exhausted, stop listening to it and move on.
+    if (done) {
+      nextPs[iterableNr] = null;
+      continue;
     }
-  } else {
-    const head = new AsyncTee(iterables.shift());
-    const rest = cartesian(...iterables);
-    for await (let items of rest) {
-      for await (let item of head) {
-        yield [item, ...items];
-      }
-    }
+
+    // Produce all combinations of the received value with the logged values
+    // from the other iterables.
+    const arrays = [...logs];
+    arrays[iterableNr] = [value];
+    const combinations = cartesianArrays(arrays);
+
+    // Append the received value to the right log.
+    logs[iterableNr] = [...logs[iterableNr], value];
+
+    // Start listening for the next value.
+    nextPs[iterableNr] = iterators[iterableNr]
+      .next()
+      .then(async ({ value, done }) => ({ value: await value, done }))
+      .then(({ value, done }) => ({ value, done, iterableNr }));
+
+    // Yield each of the produced combinations separately.
+    yield* combinations;
   }
 }
 
@@ -50,3 +89,4 @@ async function test() {
   }
   // TODO assert that result equals expected.
 }
+test();
