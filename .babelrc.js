3d527b05abbe09e6cfc392067a3646c53311d13c
Randall Leeds <randall@apache.org>
2017-11-12 17:00:15 -0800

    Use .babelrc.js instead of .babelrc
    Using JS for the .babelrc file seems to be the way the babel project is
    heading.

---
+++ .babelrc.js
@@ -0,0 +1,37 @@
+// Options for the @babel/transform-runtime plugin.
+let runtimeOptions = {
+  // Do not polyfill; leave that to applications.
+  polyfill: false,
+  // Do not import polyfills for helpers.
+  useBuiltIns: true,
+  // Export helpers as ES modules.
+  useESModules: true,
+};
+
+// Options for the @babel/env preset.
+let envOptions = {
+  // Do not transform modules.
+  modules: false,
+  // Enabled proposals that have shipped in browsers.
+  shippedProposals: true,
+  // Set target environments.
+  targets: {
+    // Browsers: > 1%, last 2 versions, Firefox ESR
+    browsers: ['defaults'],
+    // Node: LTS
+    node: '6.0',
+  },
+  // Use a minimal @babel/polyfill.
+  useBuiltIns: 'entry',
+};
+
+const config = {
+  plugins: [
+    ['@babel/transform-runtime', runtimeOptions],
+  ],
+  presets: [
+    ['@babel/env', envOptions],
+  ],
+};
+
+module.exports = config;
