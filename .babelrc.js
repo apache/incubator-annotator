bf996dc9c59a0724bbd2ce3d8903138fd8119acd
Randall Leeds <randall@apache.org>
2017-12-02 14:13:22 -0800

    Remove coverage for now
    New warnings being emitted along with the imperfect hacks to get
    istanbul working on babel 7 are not worth it at this stage of the
    project. Coverage can be added back when nyc upgrades to babel 7.

--- .babelrc.js
+++ .babelrc.js
@@ -23,36 +23,6 @@ let runtimeOptions = {
   useESModules: process.env.BABEL_ENV !== 'cjs',
 };
 
-// Hacks for istanbul coverage, taken from babel itself.
-// See https://github.com/babel/babel/pull/6382
-// This works around https://github.com/istanbuljs/istanbuljs/issues/92
-function istanbulHacks() {
-  return {
-    inherits: require("babel-plugin-istanbul").default,
-    visitor: {
-      Program: {
-        exit: function(path) {
-          if (!this.__dv__) return
-
-          const node = path.node.body[0];
-          if (
-            node.type !== "VariableDeclaration" ||
-            node.declarations[0].id.type !== "Identifier" ||
-            !node.declarations[0].id.name.match(/cov_/) ||
-            node._blockHoist !== 3
-          ) {
-            throw new Error("Something has gone wrong in the istanbul hacks.");
-          }
-
-          // Gross hacks to put the code coverage block above all compiled
-          // import statement output.
-          node._blockHoist = 5;
-        },
-      },
-    },
-  };
-}
-
 // Options for the @babel/env preset.
 let envOptions = {
   // Transform modules if compiling for production.
@@ -73,7 +43,6 @@ let envOptions = {
 const config = {
   plugins: [
     ['@babel/transform-runtime', runtimeOptions],
-    ...(process.env.NODE_ENV === 'production' ? [] : ['istanbul']),
   ],
   presets: [
     ['@babel/env', envOptions],
