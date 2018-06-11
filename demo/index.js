57a74fbd132eee7c836d86d7b12b7c73ca68b1a5
Randall Leeds <randall@apache.org>
2018-06-11 08:46:15 -0700

    Remove commented-out code

--- demo/index.js
+++ demo/index.js
@@ -18,7 +18,6 @@
 import '@babel/polyfill';
 import * as fragment from '@annotator/fragment-identifier';
 import { describeTextQuoteByRange as describeRange } from '@annotator/dom';
-// import { createAnySelector } from '@annotator/any';
 import { mark } from './mark.js';
 import { search } from './search.js';
 
