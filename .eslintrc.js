d1eb36a0d19a06ba080f63b73e15221523a26f38
Randall Leeds <randall@apache.org>
2019-05-23 14:33:26 -0400

    Set ESLint module source type and implied strict

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,7 +10,11 @@ module.exports = {
   },
   parser: 'babel-eslint',
   parserOptions: {
+    ecmaFeatures: {
+      impliedStrict: true,
+    },
     ecmaVersion: '2019',
+    sourceType: 'module',
   },
   plugins: ['import', 'prettier'],
   rules: {
