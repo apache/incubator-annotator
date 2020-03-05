7a62abc8fb5f363c2241148c31833947533b51b1
Gerben <gerben@treora.com>
2020-03-06 00:06:10 +0100

    Use @dom/highlightRange in demo

--- demo/index.js
+++ demo/index.js
@@ -28,62 +28,12 @@ import {
   createRangeSelectorCreator,
   createTextQuoteSelector,
   describeTextQuote,
+  highlightRange,
 } from '@annotator/dom';
 import { makeRefinable } from '@annotator/selector';
 
 function clear() {
   corpus.innerHTML = selectable.innerHTML;
-}
-
-function highlight(range) {
-  for (const node of textNodes(range)) {
-    const mark = document.createElement('mark');
-    const markRange = document.createRange();
-    markRange.selectNode(node);
-    markRange.surroundContents(mark);
-  }
-}
-
-function textNodes(range) {
-  const nodes = [];
-
-  if (range.collapsed) return nodes;
-
-  let startNode = range.startContainer;
-  let startOffset = range.startOffset;
-
-  if (startNode.nodeType === 3) {
-    if (startOffset > 0 && startOffset < startNode.length) {
-      startNode = startNode.splitText(startOffset);
-      startOffset = 0;
-    }
-  }
-
-  let endNode = range.endContainer;
-  let endOffset = range.endOffset;
-
-  if (endNode.nodeType === 3) {
-    if (endOffset > 0 && endOffset < endNode.length) {
-      endNode = endNode.splitText(endOffset);
-      endOffset = 0;
-    }
-  }
-
-  const walker = document.createTreeWalker(document.documentElement);
-  walker.currentNode = startNode;
-
-  while (walker.currentNode !== endNode) {
-    if (walker.currentNode.nodeType === 3) {
-      nodes.push(walker.currentNode);
-    }
-    walker.nextNode();
-  }
-
-  if (endNode.nodeType === 3 && endOffset > 0) {
-    nodes.push(endNode);
-  }
-
-  return nodes;
 }
 
 const createSelector = makeRefinable(selector => {
@@ -114,7 +64,7 @@ const refresh = async () => {
   }
 
   for (const range of ranges) {
-    highlight(range);
+    highlightRange(range);
   }
 
   parsed.innerText = JSON.stringify(selector, null, 2);
