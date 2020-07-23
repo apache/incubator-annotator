f348d4dd87d362708157b3164be828eba6c766b4
Randall Leeds <randall@apache.org>
2020-07-22 23:45:56 -0700

    Fix lexical scope in switch case

--- packages/dom/test/text-quote/match.test.ts
+++ packages/dom/test/text-quote/match.test.ts
@@ -204,9 +204,10 @@ async function testMatcher(
 
 function prettyNodeName(node: Node) {
   switch (node.nodeType) {
-    case Node.TEXT_NODE:
+    case Node.TEXT_NODE: {
       const text = (node as Text).nodeValue || '';
       return `#text "${text.length > 50 ? text.substring(0, 50) + '…' : text}"`;
+    }
     case Node.ELEMENT_NODE:
       return `<${(node as Element).tagName.toLowerCase()}>`;
     default:
