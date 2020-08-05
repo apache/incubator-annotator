bca65b945b747d190c173b26e1de1efa81e831a9
Randall Leeds <randall@apache.org>
2020-08-04 22:12:09 -0700

    Export all types from @annotator/selector

--- packages/selector/src/index.ts
+++ packages/selector/src/index.ts
@@ -18,9 +18,10 @@
  * under the License.
  */
 
-import type { Selector, Matcher } from './types';
+import type { Matcher, Selector } from './types';
 
-export type { Selector, Matcher };
+export type { Matcher, Selector } from './types';
+export type { CssSelector, RangeSelector, TextQuoteSelector } from './types';
 
 export function makeRefinable<
   // Any subtype of Selector can be made refinable; but note we limit the value
