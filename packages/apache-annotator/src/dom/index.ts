c4fedab46608e0a7b90b2fde78f76f54983e95df
Lina Ceballos <lina.ceballos@fsfe.org>
2021-03-23 13:23:39 +0100

    fixing header

--- packages/apache-annotator/src/dom/index.ts
+++ packages/apache-annotator/src/dom/index.ts
@@ -1,3 +1,6 @@
-export * from '@apache-annotator/dom';
-// SPDX-FileCopyrightText: 2016-2020 The Apache Software Foundation
-// SPDX-License-Identifier: Apache-2.0
+/*
+ * SPDX-FileCopyrightText: 2016-2020 The Apache Software Foundation
+ * SPDX-License-Identifier: Apache-2.0
+ */
+ export * from '@apache-annotator/dom';
+
