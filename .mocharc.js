24c41faa424e7d9e75c0547b8e7a16c679f23bc2
Randall Leeds <randall@apache.org>
2021-04-03 22:05:34 -0700

    Upgrade global-jsdom from v6.1.0 to v8.0.0

--- .mocharc.js
+++ .mocharc.js
@@ -21,6 +21,6 @@
 module.exports = {
   extension: ['.ts'],
   ignore: ['node_modules'],
-  require: ['./babel-register.js', 'global-jsdom/lib/register'],
+  require: ['./babel-register.js', 'global-jsdom/register'],
   timeout: 5000,
 };
