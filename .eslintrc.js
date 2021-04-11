21ecff21024b40f6603c8ba1af66f09d8b607290
Randall Leeds <randall@apache.org>
2021-04-11 15:38:53 -0700

    Enable experimental support for linting TypeScript project references
    This change makes it possible to get correct linting results without
    building the projects.

--- .eslintrc.js
+++ .eslintrc.js
@@ -111,6 +111,7 @@ module.exports = {
         ecmaVersion: 2020,
         project: ['./tsconfig.test.json', './packages/*/tsconfig.json'],
         tsconfigRootDir: __dirname,
+        EXPERIMENTAL_useSourceOfProjectReferenceRedirect: true,
       },
       plugins: ['@typescript-eslint'],
       rules: {
