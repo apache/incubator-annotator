0fc90d3e9563da4fafcc1ca9d150e9c6753a9f7c
Randall Leeds <randall@apache.org>
2021-04-18 14:48:02 -0700

    Add watch files to mocha configuration

--- .mocharc.js
+++ .mocharc.js
@@ -23,4 +23,9 @@ module.exports = {
   ignore: ['node_modules'],
   require: ['./babel-register.js', 'global-jsdom/register'],
   timeout: 5000,
+  watchFiles: [
+    './test/**/*.ts',
+    './packages/*/src/**/*.ts',
+    './packages/*/test/**/*.ts',
+  ],
 };
